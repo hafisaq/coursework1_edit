62a60b290f935381dff4231a1eaab4f439e3787b
aqhafis <hafis.firosh@rakbank.ae>
2022-11-24 23:39:45 +0400

    commit

---
+++ products.js
@@ -0,0 +1,46 @@
+let products = [
+    {
+        "id":1,
+        "title":"Maths Tuition ",
+        "description":"Math tuition after school hours to get improve the students skills",
+        "price":200,
+        "image":"images/math.png",
+        "availableInventory":10,
+        "space":10,
+        "location": "Dubai",
+        "rating" : 3
+    },
+    {
+        "id":2,
+        "title":"Physics Tuition ",
+        "description":"Physics tuition after school hours to get improve the students skills",
+        "price": 250,
+        "image":"images/math.png",
+        "availableInventory":10,
+        "space":10,
+        "location": "RAK",
+        "rating" : 4
+    },
+    {
+        "id":3,
+        "title":"Bio Tuition ",
+        "description":"Bio tuition after school hours to get improve the students skills",
+        "price":300,
+        "image":"images/math.png",
+        "availableInventory":10,
+        "space":10,
+        "location": "Dubai",
+        "rating" : 2
+    },
+    {
+        "id":4,
+        "title":"Chem  Tuition ",
+        "description":"Chem tuition after school hours to get improve the students skills",
+        "price":200,
+        "image":"images/math.png",
+        "availableInventory":10,
+        "space":10,
+        "location": "Dubai",
+        "rating" : 4
+    }
+]
